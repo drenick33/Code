f707ff2ed7ae46a3b99a1ac94030051031bdb963
drenick33 <drenick33@gmail.com>
2020-07-18 15:09:18 -0400

    Update widget tutorial things

--- Flutter/MyFirstApp/lib/main.dart
+++ Flutter/MyFirstApp/lib/main.dart
@@ -9,7 +9,9 @@ void main() {
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //Create and display many different widgets
     return Scaffold(
+      //Create an AppBar on the top
       appBar: AppBar(
         title: Text(
           "I love Sasha!",
@@ -21,12 +23,102 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.pink[200],
       ),
+
+      //Expanded Widgets: Create flex, can help contain images with other elements
+      body: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Image.asset("assets/dash.jpg"),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(42.0),
+              color: Colors.pink[50],
+              child: Text("I"),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: EdgeInsets.all(42.0),
+              color: Colors.pink[100],
+              child: Text("Love"),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: Container(
+              padding: EdgeInsets.all(42.0),
+              color: Colors.pink[200],
+              child: Text("Sasha"),
+            ),
+          )
+        ],
+      ),
+
+      //Create a Column, have a row in the column
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   crossAxisAlignment: CrossAxisAlignment.center,
+      //   children: <Widget>[
+      //     Text("I love Sasha"),
+      //     Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //       crossAxisAlignment: CrossAxisAlignment.end,
+      //       children: <Widget>[
+      //         Text("I love Sasha"),
+      //         RaisedButton(
+      //           onPressed: () {},
+      //           color: Colors.pink[50],
+      //           child: Text("Sasha is Sexy"),
+      //         ),
+      //         Container(
+      //           color: Colors.pink[100],
+      //           padding: EdgeInsets.all(42),
+      //           child: Text("Sasha in a Box"),
+      //         ),
+      //       ],
+      //     ),
+      //     RaisedButton(
+      //       onPressed: () {},
+      //       color: Colors.pink[50],
+      //       child: Text("Sasha is Sexy"),
+      //     ),
+      //     Container(
+      //       color: Colors.pink[100],
+      //       padding: EdgeInsets.all(42),
+      //       child: Text("Sasha in a Box"),
+      //     ),
+      //   ],
+      // ),
+
+      //Create a Row
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      //   crossAxisAlignment: CrossAxisAlignment.end,
+      //   children: <Widget>[
+      //     Text("I love Sasha"),
+      //     RaisedButton(
+      //       onPressed: () {},
+      //       color: Colors.pink[50],
+      //       child: Text("Sasha is Sexy"),
+      //     ),
+      //     Container(
+      //       color: Colors.pink[100],
+      //       padding: EdgeInsets.all(42),
+      //       child: Text("Sasha in a Box"),
+      //     ),
+      //   ],
+      // ),
       // body:
       // Padding(
       //   padding: EdgeInsets.all(69.0),
       //   child: Text("Sexy Sasha"),
       // ),
 
+      //Create a Container
       // Container(
       //   padding: EdgeInsets.all(20.0),
       //   margin: EdgeInsets.all(420.0),
@@ -34,28 +126,29 @@ class Home extends StatelessWidget {
       //   child: Text("Sexy Sasha"),
       // ),
 
-      body: Center(
-        //   child: IconButton(onPressed: () {}, icon: Icon(Icons.alternate_email)),
-        //   //     child: RaisedButton.icon(
-        //   //   onPressed: () {},
-        //   //   icon: Icon(Icons.mail),
-        //   //   label: Text("Sasha is Sexy"),
-        //   //   color: Colors.pink[200],
-        //   // )
-        child: FlatButton(
-          onPressed: () {},
-          child: Image.asset("assets/dash.jpg"),
-          color: Colors.black,
-        ),
-        //   // child: Icon(
-        //   //   Icons.airport_shuttle,
-        //   //   color: Colors.pinkAccent,
-        //   //   size: 420,
-        //   // ),
-        //   // child: Image(
-        //   //   image: AssetImage("assets/dash.jpg"),
-        //   // ),
-      ),
+      //Create basic widgets, Icon, Buttons, Images, Text
+      // body: Center(
+      //   //   child: IconButton(onPressed: () {}, icon: Icon(Icons.alternate_email)),
+      //   //   //     child: RaisedButton.icon(
+      //   //   //   onPressed: () {},
+      //   //   //   icon: Icon(Icons.mail),
+      //   //   //   label: Text("Sasha is Sexy"),
+      //   //   //   color: Colors.pink[200],
+      //   //   // )
+      //   child: FlatButton(
+      //     onPressed: () {},
+      //     child: Image.asset("assets/dash.jpg"),
+      //     color: Colors.black,
+      //   ),
+      //   // child: Icon(
+      //   //   Icons.airport_shuttle,
+      //   //   color: Colors.pinkAccent,
+      //   //   size: 420,
+      //   // ),
+      //   // child: Image(
+      //   //   image: AssetImage("assets/dash.jpg"),
+      //   // ),
+      // ),
       floatingActionButton: FloatingActionButton(
           onPressed: () {},
           backgroundColor: Colors.pink[200],
