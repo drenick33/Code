cceb6f8eadec0733e2a87d086959b9addc4e2d43
drenick33 <drenick1@binghamton.edu>
2020-11-22 19:50:56 -0600

    removed unused function

--- React/reader/src/components/pages/browser/queries.ts
+++
@@ -1,8 +0,0 @@
-import { get, patch, post, del } from '../../../utils/httpMethods';
-import { connect } from 'react-redux';
-
-export async function queryGetAllStories(): Promise<any> {
-  const data = await get({ url: `/story` });
-  console.log(data);
-  return data;
-}
